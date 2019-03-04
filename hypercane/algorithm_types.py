6b8e7eaa99ca1bf756a765ab996ef28c04ee4ee7
Shawn M. Jones <jones.shawn.m@gmail.com>
2019-03-04 15:54:19 -0700

    updates to allow the user to specify the number of mementos

--- hypercane/algorithm_types.py
+++ hypercane/algorithm_types.py
@@ -1,18 +1,18 @@
 
-from .algorithms.random import Random20Algorithm
-from .algorithms.first20seedmementos import First20SeedMementos
+from .algorithms.random import RandomNAlgorithm
+from .algorithms.first20seedmementos import FirstNSeedMementos
 
 supported_algorithms = {
     # "dsa1": DSA1Algorithm,
-    "first20": First20SeedMementos,
-    "random20": Random20Algorithm
+    "firstn": FirstNSeedMementos,
+    "randomn": RandomNAlgorithm
 }
 
 full_download_required = {
-    "first20": False,
-    "random20": False
+    "firstn": False,
+    "randomn": False
 }
 
-def get_algorithm(algorithm_name, collection_model):
+def get_algorithm(algorithm_name, collection_model, memento_count):
 
-    return supported_algorithms[algorithm_name](collection_model)
+    return supported_algorithms[algorithm_name](collection_model, memento_count=memento_count)
