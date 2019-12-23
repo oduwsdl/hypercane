da82c1cdaf206b3b1470ec190fb3c63b4619af69
Shawn M. Jones <jones.shawn.m@gmail.com>
2019-12-22 20:34:00 -0700

    checkpointing work

--- hypercane/actions/discover.py
+++ hypercane/actions/discover.py
@@ -1,115 +1,11 @@
 import sys
 import os
 import argparse
-import logging
 import json
-
-from requests import Session
-from requests_cache import CachedSession
 
 from ..discover import list_seed_uris, generate_archiveit_urits, download_urits_and_extract_urims, list_seed_mementos, generate_collection_metadata
 from ..version import __useragent__
-
-def get_logger(appname, loglevel, logfile):
-
-    logger = logging.getLogger(appname)
-
-    if logfile == sys.stdout:
-        logging.basicConfig( 
-            format='%(asctime)s [%(levelname)s] %(name)s: %(message)s',
-            level=loglevel)
-    else:
-        logging.basicConfig( 
-            format='%(asctime)s [%(levelname)s] %(name)s: %(message)s',
-            level=loglevel,
-            filename=logfile)
-
-    return logger
-
-def calculate_loglevel(verbose=False, quiet=False):
-  
-    # verbose trumps quiet
-    if verbose:
-        return logging.DEBUG
-
-    if quiet:
-        return logging.WARNING
-
-    return logging.INFO
-
-def get_web_session(cachefile=None):
-    
-    proxies = None
-
-    http_proxy = os.getenv('HTTP_PROXY')
-    https_proxy = os.getenv('HTTPS_PROXY')
-
-    if http_proxy is not None and https_proxy is not None:
-        proxies = {
-            'http': http_proxy,
-            'https': https_proxy
-        }
-
-    if cachefile is not None:
-        # TODO: a cachefile with Redis credentials
-        session = CachedSession(cache_name=cachefile, extension='')
-    else:
-        session = Session()
-
-    session.proxies = proxies
-    session.headers.update({'User-Agent': __useragent__})
-
-    return session
-
-def process_collection_input_types(input_argument):
-
-    supported_input_types = [
-        "archiveit"
-    ]
-
-    if '=' not in input_argument:
-        raise argparse.ArgumentTypeError(
-            "no required argument supplied for input type {}\n\n"
-            "Examples:\n"
-            "for an Archive-It collection use something like\n"
-            "-i archiveit=3639"
-            .format(input_argument)
-            )
-
-    input_type, argument = input_argument.split('=') 
-
-    if input_type not in supported_input_types:
-        raise argparse.ArgumentTypeError(
-            "{} is not a supported input type, supported types are {}".format(
-                input_type, list(supported_input_types)
-                )
-            )
-
-    return input_type, argument
-
-def add_default_args(parser):
-
-    parser.add_argument('-l', '--logfile', dest='logfile',
-        default=sys.stdout,
-        help="The path to a logging file. The log is printed to screen by default.")
-
-    parser.add_argument('-v', '--verbose', dest='verbose',
-        action='store_true',
-        help="This will raise the logging level to debug for more verbose output")
-
-    parser.add_argument('-q', '--quiet', dest='quiet',
-        action='store_true',
-        help="This will lower the logging level to only show warnings or errors")
-
-    parser.add_argument('-cf', '--cachefile', dest='cachefile',
-        default='/tmp/hypercane-cache.sqlite',
-        help="A SQLite file for use as a cache."
-    )
-
-    parser.add_argument('--version', action='version', 
-        version="{}".format(__useragent__))
-
-    return parser
+from . import get_logger, calculate_loglevel, get_web_session, add_default_args, process_collection_input_types
 
 def process_discover_seeds_args(args):
     
@@ -194,7 +90,6 @@ def discover_timemaps(args):
     logger.info("Collection identifier: {}".format(collection_id))
 
     seeds = list_seed_uris(collection_id, session)
-
     urits = generate_archiveit_urits(collection_id, seeds)
 
     with open(args.output_filename, 'w') as output:
--- hypercane/actions/sample.py
+++ hypercane/actions/sample.py
@@ -1,8 +1,57 @@
+import argparse
+import random
 
-def sample_dummy_algorithm():
-    pass
+from ..discover import list_seed_mementos
+from . import get_logger, calculate_loglevel, get_web_session, add_default_args, process_collection_input_types
+
+def sample_with_true_random_args(args):
+
+    parser = argparse.ArgumentParser(
+        description="Sample random URLs from a web archive collection. Only Archive-It is supported at this time.",
+        prog="hc sample true-random"
+        )
+
+    # TODO: add support for an input file of URI-Ms, and an input file of URI-Ts
+    parser.add_argument('-i', help="the input type and identifier, only archiveit and a collection ID is supported at this time, example: -i archiveit=8788", dest='input_type', required=True, type=process_collection_input_types)
+
+    parser.add_argument('-o', required=True, help="the file to which we write output", dest='output_filename')
+
+    parser.add_argument('-n', required=False, help="the number of items to sample", default=28, dest='sample_count')
+
+    parser = add_default_args(parser)
+
+    args = parser.parse_args(args)
+
+    return args
+
+def sample_with_true_random(args):
+    
+    args = sample_with_true_random_args(args)
+
+    logger = get_logger(
+        __name__,
+        calculate_loglevel(verbose=args.verbose, quiet=args.quiet),
+        args.logfile
+    )
+
+    session = get_web_session(cachefile=args.cachefile)
+
+    logger.info("Starting random sampling of URI-Ms.")
+
+    collection_type = args.input_type[0]
+    collection_id = args.input_type[1]
+
+    logger.info("Collection type: {}".format(collection_type))
+    logger.info("Collection identifier: {}".format(collection_id))
+
+    urims = list_seed_mementos(collection_id, session)
+    sampled_urims = random.choices(urims, k=args.sample_count)
+
+    with open(args.output_filename, 'w') as output:
+        for urim in sampled_urims:
+            output.write("{}\n".format(urim))
 
 supported_commands = {
-    "dummy": sample_dummy_algorithm()
+    "true-random": sample_with_true_random
 }
 
