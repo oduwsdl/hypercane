1146c96a7ada3637dccd02ea7f94613fe644be58
Shawn M. Jones <jones.shawn.m@gmail.com>
2020-01-03 19:48:02 -0700

    partially implemented identify (VERY BROKEN)

--- hypercane/actions/identify.py
+++ hypercane/actions/identify.py
@@ -3,16 +3,13 @@ import os
 import argparse
 import json
 
+from urllib.parse import urlparse
+
 from ..identify import list_seed_uris, generate_archiveit_urits, download_urits_and_extract_urims, list_seed_mementos, generate_collection_metadata
 from ..version import __useragent__
 from . import get_logger, calculate_loglevel, get_web_session, add_default_args, process_collection_input_types
 
-def process_discover_seeds_args(args):
-    
-    parser = argparse.ArgumentParser(
-        description="Discover the seeds in a web archive collection. Only Archive-It is supported at this time.",
-        prog="hc discover seeds"
-        )
+def process_input_args(args, parser):
 
     parser.add_argument('-i', help="the input type and identifier, only archiveit and a collection ID is supported at this time, example: -i archiveit=8788", dest='input_type', required=True, type=process_collection_input_types)
 
@@ -24,54 +21,37 @@ def process_discover_seeds_args(args):
 
     return args
 
-def discover_seeds(args):
+def extract_uris_from_input(input_string):
+
+    uri_list = input_string.split(',')
+    uri_output_list = []
     
-    args = process_discover_seeds_args(args)
+    for uri in uri_list:
+        o = urlparse(uri)
+        if o.scheme == 'http' or o.scheme == 'https':
+            uri_output_list.append(uri)
+        elif o.scheme == 'file':
+            with open(o.path) as f:
+                for line in f.read():
+                    line = line.strip()
+                    uri_output_list.append(line)
+        else:
+            # assume it is a filename
+            with open(uri) as f:
+                for line in f.read():
+                    line = line.strip()
+                    uri_output_list.append(line)
 
-    logger = get_logger(
-        __name__,
-        calculate_loglevel(verbose=args.verbose, quiet=args.quiet),
-        args.logfile
-    )
-
-    session = get_web_session(cachefile=args.cachefile)
-
-    logger.info("Starting seed discovery run.")
-
-    collection_type = args.input_type[0]
-    collection_id = args.input_type[1]
-
-    logger.info("Collection type: {}".format(collection_type))
-    logger.info("Collection identifier: {}".format(collection_id))
-
-    seeds = list_seed_uris(collection_id, session)
-
-    with open(args.output_filename, 'w') as output:
-        for seed in seeds:
-            output.write("{}\n".format(seed))
-
-    logger.info("Done with seed discovery run.")
-
-def process_discover_timemaps_args(args):
-    
-    parser = argparse.ArgumentParser(
-        description="Discover the timemaps in a web archive collection. Only Archive-It is supported at this time.",
-        prog="hc discover timemaps"
-        )
-
-    parser.add_argument('-i', help="the input type and identifier, only archiveit and a collection ID is supported at this time, example: -i archiveit=8788", dest='input_type', required=True, type=process_collection_input_types)
-
-    parser.add_argument('-o', required=True, help="the file to which we write output", dest='output_filename')
-
-    parser = add_default_args(parser)
-
-    args = parser.parse_args(args)
-
-    return args
+    return uri_output_list
 
 def discover_timemaps(args):
 
-    args = process_discover_timemaps_args(args)
+    parser = argparse.ArgumentParser(
+        description="Discover the timemaps in a web archive collection. Only Archive-It is supported at this time.",
+        prog="hc identify timemaps"
+        )
+    
+    args = process_input_args(args, parser)
 
     logger = get_logger(
         __name__,
@@ -84,13 +64,22 @@ def discover_timemaps(args):
     logger.info("Starting timemap discovery run.")
 
     collection_type = args.input_type[0]
-    collection_id = args.input_type[1]
+    urits = []
 
-    logger.info("Collection type: {}".format(collection_type))
-    logger.info("Collection identifier: {}".format(collection_id))
-
-    seeds = list_seed_uris(collection_id, session)
-    urits = generate_archiveit_urits(collection_id, seeds)
+    if collection_type == "archiveit":
+        collection_id = args.input_type[1]
+        logger.info("Collection type: {}".format(collection_type))
+        logger.info("Collection identifier: {}".format(collection_id))
+        seeds = list_seed_uris(collection_id, session)
+        urits = generate_archiveit_urits(collection_id, seeds)
+    elif collection_type == "timemaps":
+        urits = extract_uris_from_input(args.input_type[1])
+    elif collection_type == "mementos":
+        urims = extract_uris_from_input(args.input_type[1])
+    elif collection_type == "original-resources":
+        raise NotImplementedError("Extracting TimeMaps from Original Resources is not implemented at this time")
+    elif collection_type == "warcs":
+        raise NotImplementedError("Extracting TimeMaps from WARCs is not implemented at this time")
 
     with open(args.output_filename, 'w') as output:
         for urit in urits:
@@ -102,7 +91,7 @@ def process_discover_seed_mementos_args(args):
     
     parser = argparse.ArgumentParser(
         description="Discover the seed mementos in a web archive collection. Only Archive-It is supported at this time.",
-        prog="hc discover timemaps"
+        prog="hc identify timemaps"
         )
 
     parser.add_argument('-i', help="the input type and identifier, only archiveit and a collection ID is supported at this time, example: -i archiveit=8788", dest='input_type', required=True, type=process_collection_input_types)
@@ -146,8 +135,8 @@ def discover_seed_mementos(args):
 def process_discover_original_resources_args(args):
     
     parser = argparse.ArgumentParser(
-        description="Discover the original resources in a web archive collection. Only Archive-It is supported at this time.",
-        prog="hc discover seeds"
+        description="Identify the original resources in a web archive collection. Only Archive-It is supported at this time.",
+        prog="hc identify seeds"
         )
 
     parser.add_argument('-i', help="the input type and identifier, only archiveit and a collection ID is supported at this time, example: -i archiveit=8788", dest='input_type', required=True, type=process_collection_input_types)
@@ -193,7 +182,7 @@ def process_discover_collection_metadata_args(args):
 
     parser = argparse.ArgumentParser(
         description="Discover the collection metadata in a web archive collection. Only Archive-It is supported at this time.",
-        prog="hc discover timemaps"
+        prog="hc identify timemaps"
         )
 
     parser.add_argument('-i', help="the input type and identifier, only archiveit and a collection ID is supported at this time, example: -i archiveit=8788", dest='input_type', required=True, type=process_collection_input_types)
@@ -235,22 +224,21 @@ def discover_collection_metadata(args):
 
 def print_usage():
 
-    print("""'hc discover' is used discover resource identifiers in a web archive collection, document collection, a list of TimeMaps, or a directory containing WARCs
+    print("""'hc identify' is used discover resource identifiers in a web archive collection, document collection, a list of TimeMaps, or a directory containing WARCs
 
     Supported commands:
-    * seeds - for discovering seeds in a web archive collection
     * timemaps - for discovering the TimeMap URI-Ts from a web archive collection
-    * seed-mementos - for discovering the seed memento URI-Ms in a web archive collection
+    * mementos - for discovering the memento URI-Ms in a web archive collection
     * original-resources - for discovering the original resource URI-Rs in a web archive collection, list of TimeMap URI-Ts, or a directory containing WARCs
     * metadata - for discovering the metadata associated with a web archive collection
 
     Examples:
     
-    hc discover seeds -i archiveit=8788 -o seed-output-file.txt
+    hc identify original-resources -i archiveit=8788 -o seed-output-file.txt
 
-    hc discover timemaps -i archiveit=8788 -o timemap-output-file.txt
+    hc identify timemaps -i archiveit=8788 -o timemap-output-file.txt
 
-    hc discover metadata -i archiveit=8788 -o collection-8788-metadata.json
+    hc identify mementos -i archiveit=8788 -o timemap-output-file.txt
     
 """)
 
--- hypercane/identify/archivecrawl.py
+++ hypercane/identify/archivecrawl.py
@@ -5,221 +5,130 @@ from scrapy.crawler import CrawlerProcess
 
 module_logger = logging.getLogger('hypercane.identify.archivecrawl')
 
-def extract_href_string(href):
+import scrapy
+import sys
+from scrapy.crawler import CrawlerProcess
+from urllib.parse import urljoin
+import logging
 
-    if type(href) == list:
-    
-        if len(href) > 0:
-    
-            href = href[0]
-    
-            if type(href) == list:
-    
-                if len(href) > 0:
-    
-                     href = href[0]
+from requests.utils import parse_header_links
 
-    return href
+module_logger = logging.getLogger()
 
 class WaybackSpiderInitializationException(Exception):
     pass
 
+class StorageObject:
+
+    def __init__(self):
+
+        self.storage = []
+
+
+    def add(self, item):
+
+        if item not in self.storage:
+            self.storage.append(item)
+
+def extract_href_string(href):
+
+    if type(href) == list:
+  
+        if len(href) > 0:
+  
+            href = href[0]
+  
+            if type(href) == list:
+  
+                if len(href) > 0:
+  
+                     href = href[0]
+
+    return href
+
+def return_urit_and_urir(link_headers):
+
+    header_links = parse_header_links(link_headers) 
+
+    for item in header_links:
+
+        if item['rel'] == 'timemap':
+            urit = item['url']
+
+        if 'original' in item['rel']:
+            # handles original latest-version and similar
+            urir = item['url']
+
+    return (urit, urir)
+
 class WaybackSpider(scrapy.Spider):
-    
+  
     def __init__(self, *args, **kwargs):
 
         try:
             self.start_urls = kwargs['start_urls']
             self.allowed_domains = kwargs['allowed_domains']
-            self.custom_settings = {
-                'DEPTH_LIMIT': kwargs['depth_limit']
-            }
             self.link_storage = kwargs['link_storage']
         except KeyError as e:
             msg = "Failed to supply argument to WaybackSpider, details: {}".format(e)
             module_logger.exception(msg)
             raise WaybackSpiderInitializationException(msg)
-`
+
     def parse(self, response):
 
-        for sel in response.xpath('//a'):
+        retrieved_link_headers = False
 
-            href = sel.xpath('@href').extract()
+        try:
 
-            href = extract_href_string(href)
+            link_headers = response.headers["link"]
+            retrieved_link_headers = True
 
-            if href is not None:
-                if href != "":
-                    self.link_storage.add(href)
+        except KeyError:
 
-        for sel in response.xpath('//a'):
-
-            href = sel.xpath('@href').extract()
-
-            href = extract_href_string(href)
-
-            if href is not None:
-                if href != "":
-                    yield response.follow(href, self.parse)
-
-# import scrapy
-# import sys
-# from scrapy.crawler import CrawlerProcess
-# from urllib.parse import urljoin
-# import logging
-
-# module_logger = logging.getLogger()
-
-# class WaybackSpiderInitializationException(Exception):
-#     pass
-
-# #class IkeaItem(scrapy.Item):
-# #
-# #    name = scrapy.Field()
-# #    link = scrapy.Field()
+            print("Failed to get Link header from {}".format(response.url))
 
 
-# #class IkeaSpider(scrapy.Spider):
-# #    name = 'ikea'
-# #
-# #    allowed_domains = ['www.ikea.com']
-# #
-# #    start_urls = ['http://www.ikea.com/']
-# #
-# #    def parse(self, response):
-# #        for sel in response.xpath('//tr/td/a'):
-# ##            item = IkeaItem()
-# ##            item['name'] = sel.xpath('text()').extract()
-# ##            item['link'] = sel.xpath('@href').extract()
-# #
-# #            yield sel.xpath('@href').extract()
+        if retrieved_link_headers is True:
+            # Mementos should have Link headers and we only crawl Mementos
 
-# class StorageObject:
+            urit, urir = return_urit_and_urir(link_headers.decode())
 
-#     def __init__(self):
+            self.link_storage.add( (urit, urir) )
 
-#         self.storage = []
-
-
-#     def add(self, item):
-
-#         if item not in self.storage:
-#             self.storage.append(item)
-
-# def extract_href_string(href):
-
-#     if type(href) == list:
+            for sel in response.xpath('//a'):
     
-#         if len(href) > 0:
+                href = sel.xpath('@href').extract()
+                href = extract_href_string(href)
     
-#             href = href[0]
-    
-#             if type(href) == list:
-    
-#                 if len(href) > 0:
-    
-#                      href = href[0]
+                if href is not None:
+                    if href != "":
+                        yield response.follow(href, self.parse)
 
-#     return href
+process = CrawlerProcess()
 
-# class ShawnSpider(scrapy.Spider):
+starting_url = sys.argv[1]
+depth_limit = sys.argv[2]
 
-#     name = 'shawnspider'
+WaybackSpider.custom_settings = {
+    'DEPTH_LIMIT': int(depth_limit)
+}
 
-#     allowed_domains = ['wayback.archive-it.org']
+link_storage = StorageObject()
 
-#     custom_settings = {
-#         'DEPTH_LIMIT': 1
-#     }
+print("starting crawl with {}".format(starting_url))
 
-#     def __init__(self, *args, **kwargs):
+print("link storage has {} items to start".format(len(link_storage.storage)))
 
-#         super(ShawnSpider, self).__init__(*args, **kwargs)
+#process.crawl(ShawnSpider, start_urls=[starting_url], link_storage=link_storage)
+process.crawl(WaybackSpider, start_urls=[starting_url], link_storage=link_storage, allowed_domains=['wayback.archive-it.org'])
 
-#         self.start_urls = kwargs['start_urls']
-#         self.link_storage = kwargs.get('link_storage')
+process.start()
 
-#         if not self.start_urls:
-#             raise Exception("Need start_urls for crawler!!!")
+print("link storage has {} items now".format(len(link_storage.storage)))
 
-#         if not self.link_storage:
-#             raise Exception("Need link storage object for crawler!!!")
+with open("output.txt", 'w') as f:
 
-#     def parse(self, response):
+    for entry in link_storage.storage:
+        f.write("{}\n".format(entry))
 
-#         for sel in response.xpath('//a'):
-
-#             href = sel.xpath('@href').extract()
-
-#             href = extract_href_string(href)
-
-#             if href is not None:
-#                 if href != "":
-#                     self.link_storage.add(urljoin(response.url, href))
-#                     yield response.follow(href, self.parse)    
-
-# class WaybackSpider(scrapy.Spider):
-    
-#     def __init__(self, *args, **kwargs):
-
-#         try:
-#             self.start_urls = kwargs['start_urls']
-#             self.allowed_domains = kwargs['allowed_domains']
-# #            self.custom_settings = {
-# #                'DEPTH_LIMIT': kwargs['depth_limit']
-# #            }
-#             self.link_storage = kwargs['link_storage']
-#         except KeyError as e:
-#             msg = "Failed to supply argument to WaybackSpider, details: {}".format(e)
-#             module_logger.exception(msg)
-#             raise WaybackSpiderInitializationException(msg)
-
-#     def parse(self, response):
-
-#         for sel in response.xpath('//a'):
-
-#             href = sel.xpath('@href').extract()
-
-#             href = extract_href_string(href)
-
-#             if href is not None:
-#                 if href != "":
-#                     self.link_storage.add(href)
-
-#         for sel in response.xpath('//a'):
-
-#             href = sel.xpath('@href').extract()
-
-#             href = extract_href_string(href)
-
-#             if href is not None:
-#                 if href != "":
-#                     yield response.follow(href, self.parse)
-
-# process = CrawlerProcess()
-
-# starting_url = sys.argv[1]
-
-# WaybackSpider.custom_settings = {
-#     'DEPTH_LIMIT': 1
-# }
-
-# link_storage = StorageObject()
-
-# print("starting crawl with {}".format(starting_url))
-
-# print("link storage has {} items to start".format(len(link_storage.storage)))
-
-# #process.crawl(ShawnSpider, start_urls=[starting_url], link_storage=link_storage)
-# process.crawl(WaybackSpider, start_urls=[starting_url], link_storage=link_storage, allowed_domains=['wayback.archive-it.org'])
-
-# process.start()
-
-# print("link storage has {} items now".format(len(link_storage.storage)))
-
-# with open("output.txt", 'w') as f:
-
-#     for entry in link_storage.storage:
-#         f.write("{}\n".format(entry))
-
-# print("done crawling")
+print("done crawling")
