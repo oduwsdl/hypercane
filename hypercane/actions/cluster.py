ac523c5a712afd8aea11bc593a18846f6dcbd248
Shawn M. Jones <jones.shawn.m@gmail.com>
2020-01-12 11:36:34 -0700

    AlNoamanay's time slice clustering

--- hypercane/actions/cluster.py
+++ hypercane/actions/cluster.py
@@ -2,8 +2,16 @@ import sys
 import os
 import argparse
 import json
+import concurrent.futures
+import math
 
-from ..actions import add_input_args, add_default_args
+from datetime import datetime
+
+from ..actions import add_input_args, add_default_args, get_logger, calculate_loglevel
+from ..identify import discover_timemaps_by_input_type, \
+    discover_mementos_by_input_type, download_urits_and_extract_urims
+from ..utils import get_web_session, get_memento_http_metadata
+from ..cluster.time_slice import execute_time_slice
 
 def process_input_args(args, parser):
 
@@ -18,10 +26,64 @@ def process_input_args(args, parser):
     return args
 
 def time_slice(args):
-    raise NotImplementedError("Time Slice Clustering Not Implemented Yet")
+    
+    parser = argparse.ArgumentParser(
+        description="Only keep documents from a collection with a specific language.",
+        prog="hc cluster time-slice"
+    )
+
+    args = process_input_args(args, parser)
+
+    logger = get_logger(
+        __name__,
+        calculate_loglevel(verbose=args.verbose, quiet=args.quiet),
+        args.logfile
+    )
+
+    logger.info("beginning time slicing of collection")
+
+    input_type = args.input_type[0]
+    input_args = args.input_type[1]
+
+    session = get_web_session(cache_storage=args.cache_storage)
+
+    urims = discover_mementos_by_input_type(
+        input_type, input_args, args.crawl_depth, session
+    )
+
+    cache_storage = args.cache_storage
+
+    logger.info("There were {} mementos discovered in the input".format(len(urims)))
+
+    slices = execute_time_slice(urims, cache_storage)
+
+    with open(args.output_filename, 'w') as f:
+
+        for i in range(0, len(slices)):
+
+            for urim in slices[i]:
+                f.write("{}\t{}\n".format(urim, i))
+
+    logger.info("finished time slicing, output is available at {}".format(args.output_filename))
 
 def dbscan(args):
     raise NotImplementedError("DBSCAN Clustering Not Implementing Yet")
+
+def print_usage():
+
+    print("""'hc cluster' is used to employ techniques to cluster a web archive collection, document collection, a list of TimeMaps, or a directory containing WARCs
+
+    Supported commands:
+    * time-slice - slice the collection into buckets by Memento-Datetime, as in AlNoamany's Algorithm
+    * dbscan - cluster the user-supplied feature using the DBSCAN algorithm
+
+    Examples:
+    
+    hc cluster time-slice -i mementos=novel-content.txt -o mdt-slices.json 
+
+    hc cluster dbscan features=tf-simhash -i clustered-mementos=mdt-slices.json -o sliced-and-clustered.json
+    
+""")
 
 supported_commands = {
     "time-slice": time_slice,
---
+++ hypercane/cluster/time_slice.py
@@ -0,0 +1,58 @@
+import concurrent.futures
+import math
+import logging
+
+from datetime import datetime
+
+from ..utils import get_memento_http_metadata
+
+module_logger = logging.getLogger('hypercane.cluster.time_slice')
+
+def execute_time_slice(urims, cache_storage):
+
+    mementos = []
+
+    # extract the memento datetimes
+    with concurrent.futures.ThreadPoolExecutor(max_workers=5) as executor:
+
+        future_to_urim = { executor.submit(get_memento_http_metadata, urim, cache_storage, metadata_fields=[ "memento-datetime"]): urim for urim in urims }
+
+        for future in concurrent.futures.as_completed(future_to_urim):
+
+            urim = future_to_urim[future]
+            mdt = future.result()[0]
+            mdt = datetime.strptime(mdt, "%a, %d %b %Y %H:%M:%S GMT")
+            mementos.append( (mdt, urim) )
+
+    # calculate the number of slices 28 + math.log(len(mementos))
+    number_of_slices = math.ceil(28 + math.log(len(mementos)))
+
+    module_logger.info("The collection will be divided into {} slices".format(number_of_slices))
+
+    # divide the number of mementos by the number of slices to determine mementos/slice
+    mementos_per_slice = math.ceil(len(mementos) / number_of_slices)
+
+    module_logger.info("There will be {} mementos in each slice".format(mementos_per_slice))
+
+    slices = []
+
+    # sort mementos by memento-datetime
+    # iterate through mementos, filling each slice
+    current_slice = []
+
+    module_logger.info("Slicing {} mementos into {} slices".format(len(mementos), number_of_slices))
+
+    for memento in sorted(mementos):
+
+        if len(current_slice) < mementos_per_slice:
+            current_slice.append(memento[1])
+        else:
+            slices.append(current_slice)
+            current_slice = []
+            current_slice.append(memento[1])
+
+    # the last slice that might be unfilled
+    if len(current_slice) > 0:
+        slices.append(current_slice)
+
+    return slices
