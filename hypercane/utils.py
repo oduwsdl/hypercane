4bd1e1fc450035098f336d2f4ebda27e1867e3a6
Shawn M. Jones <jones.shawn.m@gmail.com>
2020-05-01 19:25:11 -0600

    updates from synthesize blog post work

--- hypercane/utils.py
+++ hypercane/utils.py
@@ -12,6 +12,8 @@ from guess_language import guess_language
 from justext import justext, get_stoplist
 from simhash import Simhash
 from newspaper import Article
+from urllib3.util.retry import Retry
+from requests.adapters import HTTPAdapter
 
 from .version import __useragent__
 
@@ -44,6 +46,17 @@ def get_web_session(cache_storage=None):
             session = CachedSession(cache_name=cache_storage, extension='')
     else:
         session = Session()
+
+    retry = Retry(
+        total=10,
+        read=10,
+        connect=10,
+        backoff_factor=0.3,
+        status_forcelist=(500, 502, 504)
+    )
+    adapter = HTTPAdapter(max_retries=retry)
+    session.mount('http://', adapter)
+    session.mount('https://', adapter) 
 
     session.proxies = proxies
     session.headers.update({'User-Agent': __useragent__})
--- hypercane/version.py
+++ hypercane/version.py
@@ -1,3 +1,3 @@
 __appname__ = "hypercane"
-__appversion__ = '0.2020.04.23.022716'
+__appversion__ = '0.2020.05.02.012444'
 __useragent__ = "{}/{}".format(__appname__, __appversion__)
