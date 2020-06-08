fb1303aa1dd61289bf7c52c061cd8fb40023b3a3
Shawn M. Jones <jones.shawn.m@gmail.com>
2020-06-07 20:04:59 -0600

    fix for failed base URI download

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -26,7 +26,7 @@ author = u'Shawn M. Jones'
 # The short X.Y version
 version = u''
 # The full version, including alpha/beta/rc tags
-release = u'0.2020.06.07.163024'
+release = u'0.2020.06.08.020433'
 
 # -- General configuration ---------------------------------------------------
 
--- hypercane/version.py
+++ hypercane/version.py
@@ -1,3 +1,3 @@
 __appname__ = "hypercane"
-__appversion__ = '0.2020.06.07.163024'
+__appversion__ = '0.2020.06.08.020433'
 __useragent__ = "{}/{}".format(__appname__, __appversion__)
