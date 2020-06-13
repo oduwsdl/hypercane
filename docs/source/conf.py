b5234cf9b9a98978d7d337fe182dc8edec131d77
Shawn M. Jones <jones.shawn.m@gmail.com>
2020-06-12 23:00:09 -0600

    updated cluster help message

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -26,7 +26,7 @@ author = u'Shawn M. Jones'
 # The short X.Y version
 version = u''
 # The full version, including alpha/beta/rc tags
-release = u'0.2020.06.13.044903'
+release = u'0.2020.06.13.045949'
 
 # -- General configuration ---------------------------------------------------
 
