e9dff855cb2848e8b9754160ffcdfab8843f16be
Shawn M. Jones <jones.shawn.m@gmail.com>
2020-06-23 19:28:21 -0600

    fixes for sumgram stopwords

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -26,7 +26,7 @@ author = u'Shawn M. Jones'
 # The short X.Y version
 version = u''
 # The full version, including alpha/beta/rc tags
-release = u'0.2020.06.19.133122'
+release = u'0.2020.06.24.012712'
 
 # -- General configuration ---------------------------------------------------
 
