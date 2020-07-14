64dfa0079ac7adc72933528d99c6daff658fcee3
Shawn M. Jones <jones.shawn.m@gmail.com>
2020-07-13 20:44:17 -0600

    the number of clusters for time-slicing is now configurable

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -26,7 +26,7 @@ author = u'Shawn M. Jones'
 # The short X.Y version
 version = u''
 # The full version, including alpha/beta/rc tags
-release = u'0.2020.07.13.224110'
+release = u'0.2020.07.14.023052'
 
 # -- General configuration ---------------------------------------------------
 
--- hypercane/version.py
+++ hypercane/version.py
@@ -1,3 +1,3 @@
 __appname__ = "hypercane"
-__appversion__ = '0.2020.07.13.224110'
+__appversion__ = '0.2020.07.14.023052'
 __useragent__ = "{}/{}".format(__appname__, __appversion__)
