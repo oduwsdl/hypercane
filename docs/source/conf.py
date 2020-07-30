6b3fb10cdf191c650e3f2e1f9dbb781636806951
Shawn M. Jones <jones.shawn.m@gmail.com>
2020-07-30 04:43:22 -0600

    fix for image report download failures

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -26,7 +26,7 @@ author = u'Shawn M. Jones'
 # The short X.Y version
 version = u''
 # The full version, including alpha/beta/rc tags
-release = u'0.2020.07.21.221627'
+release = u'0.2020.07.30.104057'
 
 # -- General configuration ---------------------------------------------------
 
