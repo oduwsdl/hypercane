bf11874bade93057d36941844a36655a2e22318e
Shawn M. Jones <jones.shawn.m@gmail.com>
2020-10-23 16:59:13 -0600

    added functionality for filtering by largest clusters

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -26,7 +26,7 @@ author = u'Shawn M. Jones'
 # The short X.Y version
 version = u''
 # The full version, including alpha/beta/rc tags
-release = u'0.2020.10.23.220607'
+release = u'0.2020.10.23.225847'
 
 # -- General configuration ---------------------------------------------------
 
