d098fa804e7e854d367c22fda512f81a9ae24666
Shawn M. Jones <jones.shawn.m@gmail.com>
2021-02-20 19:34:01 -0700

    raising version

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -26,7 +26,7 @@ author = u'Shawn M. Jones'
 # The short X.Y version
 version = u''
 # The full version, including alpha/beta/rc tags
-release = u'0.2020.11.03.221237'
+release = u'0.2021.02.21.023352'
 
 # -- General configuration ---------------------------------------------------
 
