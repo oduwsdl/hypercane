f99881a28a9e7bbb12dd56288450121149f4d094
Shawn M. Jones <jones.shawn.m@gmail.com>
2021-04-05 10:41:40 -0600

    raising version number of numpy to a compatible version

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -26,7 +26,7 @@ author = u'Shawn M. Jones'
 # The short X.Y version
 version = u''
 # The full version, including alpha/beta/rc tags
-release = u'0.2021.03.29.125914'
+release = u'0.2021.04.05.164118'
 
 # -- General configuration ---------------------------------------------------
 
