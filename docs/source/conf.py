31d02e078f9e4a759a360ede5af39b4b309261b3
Shawn M. Jones <jones.shawn.m@gmail.com>
2021-05-07 16:31:29 -0600

    working embedded resource extraction for WARCs

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -26,7 +26,7 @@ author = u'Shawn M. Jones'
 # The short X.Y version
 version = u''
 # The full version, including alpha/beta/rc tags
-release = u'0.2021.05.07.005945'
+release = u'0.2021.05.07.223102'
 
 # -- General configuration ---------------------------------------------------
 
