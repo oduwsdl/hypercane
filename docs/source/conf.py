957afd0721ef6e42bad304ecd1348e7338fb727a
Shawn M. Jones <jones.shawn.m@gmail.com>
2021-05-08 20:54:28 -0600

    now images found in the metadata are added to WARCs

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -26,7 +26,7 @@ author = u'Shawn M. Jones'
 # The short X.Y version
 version = u''
 # The full version, including alpha/beta/rc tags
-release = u'0.2021.05.07.224617'
+release = u'0.2021.05.09.025334'
 
 # -- General configuration ---------------------------------------------------
 
--- hypercane/version.py
+++ hypercane/version.py
@@ -1,3 +1,3 @@
 __appname__ = "hypercane"
-__appversion__ = '0.2021.05.07.224617'
+__appversion__ = '0.2021.05.09.025334'
 __useragent__ = "{}/{}".format(__appname__, __appversion__)
