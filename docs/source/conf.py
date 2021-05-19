6160ffcf70f1e61356166092cc3e4038dd605c7a
Shawn M. Jones <jones.shawn.m@gmail.com>
2021-05-19 16:56:54 -0600

    adding card score to the scores available

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -26,7 +26,7 @@ author = u'Shawn M. Jones'
 # The short X.Y version
 version = u''
 # The full version, including alpha/beta/rc tags
-release = u'0.2021.05.19.221117'
+release = u'0.2021.05.19.225604'
 
 # -- General configuration ---------------------------------------------------
 
--- hypercane/score/card_score.py
+++ hypercane/score/card_score.py
@@ -1,4 +1,7 @@
 import logging
+import traceback
+
+import hypercane.errors
 
 module_logger = logging.getLogger('hypercane.score.card_score')
 
@@ -12,25 +15,32 @@ def compute_simple_card_score(urim, session):
 
         meta_kv_pairs = create_html_metadata_kv_pairs(urim, session)
 
+        for card_part in ['twitter:card']:
+
+            if card_part in meta_kv_pairs:
+                if len(meta_kv_pairs[card_part]) > 0:
+                    card_score += 1
+                    break
+
         for title_part in ['twitter:title', 'og:title']:
             if title_part in meta_kv_pairs:
                 if len(meta_kv_pairs[title_part]) > 0:
                     card_score += 1
                     break
 
-        for description_part in ['twitter:description', 'og:description', 'description']:
+        for description_part in ['twitter:description', 'og:description']:
             if description_part in meta_kv_pairs:
                 if len(meta_kv_pairs[description_part]) > 0:
                     card_score += 1
                     break
 
-        for image_part in ['twitter:image', 'og:image', 'og:image:url', 'twitter:image:url']:
+        for image_part in ['twitter:image', 'og:image', 'og:image:url', 'twitter:image:url', 'twitter:image:src']:
             if image_part in meta_kv_pairs:
                 if len(meta_kv_pairs[image_part]) > 0:
                     card_score += 1
                     break
 
-        card_score = card_score / 3
+        card_score = card_score / 4
 
         return card_score
 
@@ -40,5 +50,33 @@ def compute_simple_card_score(urim, session):
         return 0
         
 
+def compute_simple_card_scores(urimdata, session):
 
+    import concurrent.futures
+
+    urims = list(urimdata.keys())
+    urim_to_score = {}
+    total_urims = len(urims)
+    completed_urims = 0
+
+    with concurrent.futures.ThreadPoolExecutor(max_workers=5) as executor:
+
+        future_to_urim = { executor.submit(compute_simple_card_score, urim, session): urim for urim in urims}
+
+        for future in concurrent.futures.as_completed(future_to_urim):
+            completed_urims += 1
+            module_logger.info("extracting score result for {}/{}".format(completed_urims, total_urims))
+
+            try:
+                urim = future_to_urim[future]
+                urim_to_score[urim] = future.result()
+            except Exception as exc:
+                module_logger.exception("Error: {}, failed to compute score for {}, skipping...".format(repr(exc), urim))
+                hypercane.errors.errorstore.add(urim, traceback.format_exc())
+
+
+    for urim in urim_to_score:
+        urimdata[urim]['Rank---Card-Score'] = urim_to_score[urim]
+
+    return urimdata
     
