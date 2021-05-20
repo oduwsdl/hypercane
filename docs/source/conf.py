3644ab7f15fa7a6df266215f03941646477134c4
Shawn M. Jones <jones.shawn.m@gmail.com>
2021-05-19 19:07:56 -0600

    order by score now has some sensible defaults

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -26,7 +26,7 @@ author = u'Shawn M. Jones'
 # The short X.Y version
 version = u''
 # The full version, including alpha/beta/rc tags
-release = u'0.2021.05.20.010220'
+release = u'0.2021.05.20.010732'
 
 # -- General configuration ---------------------------------------------------
 
--- hypercane/version.py
+++ hypercane/version.py
@@ -1,3 +1,3 @@
 __appname__ = "hypercane"
-__appversion__ = '0.2021.05.20.010220'
+__appversion__ = '0.2021.05.20.010732'
 __useragent__ = "{}/{}".format(__appname__, __appversion__)
