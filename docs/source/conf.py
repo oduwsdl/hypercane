05f1571491a6e6e3b78de46c4b4e98894b506294
Shawn M. Jones <jones.shawn.m@gmail.com>
2021-05-20 14:22:54 -0600

    random oversampling

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -26,7 +26,7 @@ author = u'Shawn M. Jones'
 # The short X.Y version
 version = u''
 # The full version, including alpha/beta/rc tags
-release = u'0.2021.05.20.192609'
+release = u'0.2021.05.20.202241'
 
 # -- General configuration ---------------------------------------------------
 
--- hypercane/actions/sample.py
+++ hypercane/actions/sample.py
@@ -519,6 +519,57 @@ def sample_with_random_cluster(args):
 
     logger.info("Done sampling.")
 
+def sample_with_random_oversample(args):
+
+    from hypercane.sample.probability import select_by_random_oversampling
+    from hypercane.actions import get_logger, calculate_loglevel
+    from hypercane.utils import get_web_session, save_resource_data, organize_mementos_by_cluster
+    from hypercane.identify import discover_resource_data_by_input_type, \
+        discover_mementos_by_input_type
+    import argparse
+    from hypercane.actions import add_input_args, add_default_args
+
+    parser = argparse.ArgumentParser(
+        description="Sample random URLs from a web archive collection.",
+        prog="hc sample random-oversample"
+        )
+
+    parser = add_input_args(parser)
+    parser = add_default_args(parser)
+
+    args = parser.parse_args(args)
+
+    logger = get_logger(
+        __name__,
+        calculate_loglevel(verbose=args.verbose, quiet=args.quiet),
+        args.logfile
+    )
+
+    if args.errorfilename is not None:
+        hypercane.errors.errorstore.type = hypercane.errors.FileErrorStore(args.errorfilename)
+
+    session = get_web_session(cache_storage=args.cache_storage)
+    output_type = 'mementos'
+
+    logger.info("Starting random sampling of URI-Ms.")
+
+    urimdata = discover_resource_data_by_input_type(
+        args.input_type, output_type, args.input_arguments, args.crawl_depth,
+        session, discover_mementos_by_input_type
+    )
+
+    memento_clusters = organize_mementos_by_cluster(urimdata)
+
+    logger.info("Executing random oversample from {} clusters of {} URI-Ms".format(
+        len(memento_clusters), len(urimdata.keys())))
+    
+    sampled_urims = select_by_random_oversampling(memento_clusters)
+
+    logger.info("Writing {} sampled URI-Ms out to {}".format(len(sampled_urims), args.output_filename))
+    save_resource_data(args.output_filename, urimdata, 'mementos', sampled_urims)
+
+    logger.info("Done sampling.")
+
 def print_usage():
 
     print("""hc sample is used execute different algorithms for selecting mementos from a web archive collection, document collection, a list of TimeMaps, or a directory containing WARCs
@@ -532,6 +583,8 @@ def print_usage():
     * stratified-random - returns j items randomly chosen from each cluster, requries that the input be clustered with the cluster action
     * stratified-systematic - returns every jth URI-M from each cluster, requries that the input be clustered witht he cluster action
     * random-cluster - return j randomly selected clusters from the sample, requires that the input be clustered with the cluster action
+    * random-oversample - randomly duplicates URI-Ms in the smaller clusters until they match the size of the largest cluster, requires input be clustered with the cluster action
+
 
     Examples:
 
@@ -550,12 +603,11 @@ supported_commands = {
     "stratified-random": sample_with_stratified_random,
     "stratified-systematic": sample_with_stratified_systematic,
     "random-cluster": sample_with_random_cluster,
-    # "random-oversample": None,
+    "random-oversample": sample_with_random_oversample,
     # "random-undersample": None
 }
 
 
-    # * random-oversample - randomly duplicates URI-Ms in the smaller clusters until they match the size of the largest cluster, requires input be clustered with the cluster action
     # * random-undersample - randomly chooses URI-Ms from the larger clusters until they match the size of the smallest cluster, requires input be clustered with the cluster action
 
 
