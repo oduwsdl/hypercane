718c8bb03434dbf2496aa846eda6704ff137538a
Shawn M. Jones <jones.shawn.m@gmail.com>
2021-06-06 21:39:51 -0600

    fix for META redirect errors returned by MementoEmbed

--- hypercane/identify/__init__.py
+++ hypercane/identify/__init__.py
@@ -25,7 +25,7 @@ from .archivecrawl import crawl_mementos, StorageObject, crawl_live_web_resource
 from ..utils import process_input_for_cluster_and_rank, get_memento_http_metadata
 import hypercane.errors
 from hypercane.version import __useragent__
-from mementoembed.mementoresource import MementoURINotAtArchiveFailure
+from mementoembed.mementoresource import MementoURINotAtArchiveFailure, MementoMetaRedirectParsingError
 
 module_logger = logging.getLogger('hypercane.identify')
 
@@ -748,6 +748,8 @@ def generate_faux_urit(urim, cache_storage):
         module_logger.exception("Could not discover original resource for [{}], skipping original resource discovery, reporting exception...".format(urim))
     except RetryError:
         module_logger.exception("Exceeded the number of retries for [{}] , skipping original resource discovery, reporting exception...".format(urim))
+    except MementoMetaRedirectParsingError:
+        module_logger.exception("Could not process the redirection URL in the META tag for [{}] , skipping original resource discovery, reporting exception...".format(urim))
 
     return faux_urit
 
