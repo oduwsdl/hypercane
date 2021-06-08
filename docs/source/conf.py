5ef976334bc10b7f8933343ec92ec3bd425acb66
Shawn M. Jones <jones.shawn.m@gmail.com>
2021-06-08 17:42:15 -0600

    improved query generation from collection metadata

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -26,7 +26,7 @@ author = u'Shawn M. Jones'
 # The short X.Y version
 version = u''
 # The full version, including alpha/beta/rc tags
-release = u'0.2021.06.08.223401'
+release = u'0.2021.06.08.234200'
 
 # -- General configuration ---------------------------------------------------
 
