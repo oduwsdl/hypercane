785164b5ee42b94119774a1c8a02c926700a8708
Shawn M. Jones <jones.shawn.m@gmail.com>
2021-06-13 18:36:46 -0600

    fix for DSA2 score, raising version of otmt

--- hypercane/hfilter/highest_rank_per_cluster.py
+++ hypercane/hfilter/highest_rank_per_cluster.py
@@ -13,12 +13,16 @@ def return_highest_ranking_memento_per_cluster(urimdata, rankkey):
 
     module_logger.debug("cluster_assignments: {}".format(cluster_assignments))
     module_logger.debug("urimdata: {}".format(urimdata))
+    module_logger.info("Applying score key {} to finding highest scoring memento per cluster for {} URI-Ms".format(
+        rankkey, len(urimdata)
+    ))
 
     for cluster in cluster_assignments:
 
         cluster_ranks = []
 
         for urim in cluster_assignments[cluster]:
+            module_logger.info("examining URI-M {} with record {}".format(urim, urimdata[urim]))
             cluster_ranks.append( (urimdata[urim][rankkey], urim) )
 
         highest_rank_per_cluster.append( max(cluster_ranks)[1] )
