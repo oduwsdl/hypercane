314221aaa737cf5a12fda7a0028d08fa854d6536
Shawn M. Jones <jones.shawn.m@gmail.com>
2021-08-01 00:55:40 -0600

    new order cli

---
+++ hypercane/cli/order.py
@@ -0,0 +1,47 @@
+import hypercane.actions.order
+import hypercane.errors
+
+from hypercane.args import universal_cli_required_args, universal_cli_optional_args
+from hypercane.args.order import order_parser
+from hypercane.version import __useragent__
+from hypercane.actions import get_logger, calculate_loglevel
+
+order_functions = {
+    "pubdate-else-memento-datetime": hypercane.actions.order.pubdate_else_memento_datetime,
+    "memento-datetime": hypercane.actions.order.memento_datetime,
+    "score": hypercane.actions.order.score_sort,
+}
+
+if __name__ == '__main__':
+
+    for item in order_parser._subparsers._group_actions:
+        for key in item.choices:
+
+            subparser = item.choices[key]
+
+            # Wooey's install script does not know how to handle functions, so we have to repeat this
+            required = subparser.add_argument_group('required arguments')
+
+            for entry in universal_cli_required_args:
+                flags = entry['flags']
+                argument_params = entry['argument_params']
+                required.add_argument(*flags, **argument_params)
+
+            optional = subparser.add_argument_group('optional arguments')
+            for entry in universal_cli_optional_args:
+                flags = entry['flags']
+                argument_params = entry['argument_params']
+                optional.add_argument(*flags, **argument_params)
+
+    args = order_parser.parse_args()
+
+    logger = get_logger(
+        __name__,
+        calculate_loglevel(verbose=args.verbose, quiet=args.quiet),
+        args.logfile
+    )
+
+    if args.errorfilename is not None:
+        hypercane.errors.errorstore.type = hypercane.errors.FileErrorStore(args.errorfilename)
+
+    order_functions[args.which](args)
