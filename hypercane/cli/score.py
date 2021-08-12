b6faf951521083c704f15465c502f247d3e3e844
Shawn M. Jones <jones.shawn.m@gmail.com>
2021-08-11 22:37:27 -0600

    updating score CLI to reduce redundancy #42

--- hypercane/cli/score.py
+++ hypercane/cli/score.py
@@ -1,23 +1,9 @@
-import hypercane.actions.score
 import hypercane.errors
 
 from hypercane.args import universal_cli_required_args, universal_cli_optional_args
 from hypercane.args.score import score_parser
 from hypercane.version import __useragent__
 from hypercane.actions import get_logger, calculate_loglevel
-
-score_functions = {
-    "dsa1-scoring": hypercane.actions.score.dsa1_scoring,
-    "bm25": hypercane.actions.score.bm25_ranking,
-    "image-count": hypercane.actions.score.image_count_scoring,
-    "simple-card-score": hypercane.actions.score.simple_card_scoring,
-    "path-depth": hypercane.actions.score.path_depth_scoring,
-    "url-category-score": hypercane.actions.score.category_scoring,
-    "top-entities-and-bm25": hypercane.actions.score.score_by_top_entities_and_bm25,
-    "distance-from-centroid": hypercane.actions.score.score_by_distance_from_centroid,
-    "size" : hypercane.actions.score.score_by_size,
-    "dsa2-scoring": hypercane.actions.score.dsa2_scoring
-}
 
 if __name__ == '__main__':
 
@@ -51,4 +37,4 @@ if __name__ == '__main__':
     if args.errorfilename is not None:
         hypercane.errors.errorstore.type = hypercane.errors.FileErrorStore(args.errorfilename)
 
-    score_functions[args.which](args)
+    args.exec(args)
