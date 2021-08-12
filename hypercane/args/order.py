717da6f0f59a417931233991f47df4f0d6fd6743
Shawn M. Jones <jones.shawn.m@gmail.com>
2021-08-11 22:32:07 -0600

    updating order CLI to reduce redundancy #43

--- hypercane/args/order.py
+++ hypercane/args/order.py
@@ -1,6 +1,8 @@
 import argparse
 from argparse import RawTextHelpFormatter
 
+import hypercane.actions.order
+
 order_parser = argparse.ArgumentParser(prog="hc order",
     description="'hc order' orders the memento list input by some feature or function.",
     formatter_class=RawTextHelpFormatter
@@ -10,13 +12,22 @@ subparsers = order_parser.add_subparsers(help='ordering methods', dest="ordering
 subparsers.required = True
 
 pubdate_parser = subparsers.add_parser('pubdate-else-memento-datetime', help="order the documents according to AlNoamany's Algorithm")
-pubdate_parser.set_defaults(which='pubdate-else-memento-datetime')
+pubdate_parser.set_defaults(
+    which='pubdate-else-memento-datetime',
+    exec=hypercane.actions.order.pubdate_else_memento_datetime
+)
 
 mementodatetime_parser = subparsers.add_parser('memento-datetime', help="order the documents by memento-datetime")
-mementodatetime_parser.set_defaults(which='memento-datetime')
+mementodatetime_parser.set_defaults(
+    which='memento-datetime',
+    exec=hypercane.actions.order.memento_datetime
+)
 
 score_parser = subparsers.add_parser('score', help="order the documents by score")
-score_parser.set_defaults(which='score')
+score_parser.set_defaults(
+    which='score',
+    exec=hypercane.actions.order.score_sort
+)
 
 score_parser.add_argument('--descending', help="If specified, sort such that highest scoring URI-Ms are first.",
     action='store_true', default=False, dest='descending'
