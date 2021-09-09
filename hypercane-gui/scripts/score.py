f2c5cc6b962875da2cb9a1af5dbdc969eb75899b
Shawn M. Jones <jones.shawn.m@gmail.com>
2021-09-08 18:14:52 -0600

    score GUI script (#42) now confoms to style of #41, #43, #44, #45, #46

--- hypercane-gui/scripts/score.py
+++ hypercane-gui/scripts/score.py
@@ -8,19 +8,6 @@ from hypercane.args.score import score_parser
 from hypercane.version import __useragent__
 from hypercane.actions import get_logger, calculate_loglevel
 
-score_functions = {
-    "dsa1-scoring": hypercane.actions.score.dsa1_scoring,
-    "bm25": hypercane.actions.score.bm25_ranking,
-    "image-count": hypercane.actions.score.image_count_scoring,
-    "simple-card-score": hypercane.actions.score.simple_card_scoring,
-    "path-depth": hypercane.actions.score.path_depth_scoring,
-    "url-category-score": hypercane.actions.score.category_scoring,
-    "top-entities-and-bm25": hypercane.actions.score.score_by_top_entities_and_bm25,
-    "distance-from-centroid": hypercane.actions.score.score_by_distance_from_centroid,
-    "size" : hypercane.actions.score.score_by_size,
-    "dsa2-scoring": hypercane.actions.score.dsa2_scoring
-}
-
 if __name__ == '__main__':
 
     for item in score_parser._subparsers._group_actions:
@@ -61,5 +48,5 @@ if __name__ == '__main__':
         hypercane.errors.errorstore.type = hypercane.errors.FileErrorStore(args.errorfilename)
 
     print("starting to score {} in input".format(args.which))
-    score_functions[args.which](args)
+    args.exec(args)
     print("done scoring with {}".format(args.which))
