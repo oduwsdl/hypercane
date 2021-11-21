3e93114b664b015a15de1fc7becc2710c61c6c66
Shawn M. Jones <jones.shawn.m@gmail.com>
2021-11-20 22:11:31 -0700

    fixes to installation test environment for Ubuntu #53

--- test/installer/run-ubuntu-install-start-shell.sh
+++ test/installer/run-ubuntu-install-start-shell.sh
@@ -12,13 +12,14 @@ docker exec -it ubuntu2104test apt-get update -y
 
 echo installing MongoDB
 docker exec -it ubuntu2104test apt-get install -y wget gnupg
-docker exec -it ubuntu2104test "wget -O /tmp/server-5.0.asc https://www.mongodb.org/static/pgp/server-5.0.asc"
-docker exec -it ubuntu2104test "apt-key add /tmp/server-5.0.asc"
-docker cp ${SCRIPT_DIR}/../ubuntu2104/mongodb-org-5.0.list ubuntu2104:/etc/apt/sources.list.d/mongodb-org-5.0.list
-docker exec -it ubuntu2104 apt-get install -y mongodb-org
-docker exec -it ubuntu2104 systemctl enable mongod.service
-docker exec -it ubuntu2104 systemctl start mongod.service
-# docker exec -it ubuntu2104test 'echo "deb [ arch=amd64,arm64 ] https://repo.mongodb.org/apt/ubuntu focal/mongodb-org/5.0 multiverse" | tee /etc/apt/sources.list.d/mongodb-org-5.0.list'
+docker exec -it ubuntu2104test wget -O server-5.0.asc https://www.mongodb.org/static/pgp/server-5.0.asc
+docker exec -it ubuntu2104test apt-key add server-5.0.asc
+docker cp ${SCRIPT_DIR}/ubuntu2104/mongodb-org-5.0.list ubuntu2104test:/etc/apt/sources.list.d/mongodb-org-5.0.list
+docker exec -it ubuntu2104test chown root:root /etc/apt/sources.list.d/mongodb-org-5.0.list
+docker exec -it ubuntu2104test apt-get update -y
+docker exec -it ubuntu2104test apt-get install -y mongodb-org
+docker exec -it ubuntu2104test systemctl enable mongod.service
+docker exec -it ubuntu2104test systemctl start mongod.service
 
 echo copying $DEBFILE to container /root/ directory
 docker cp $DEBFILE ubuntu2104test:/root
