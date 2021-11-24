933d6f8571e8075f3d25418ac473a6a471c6c3ef
Shawn M. Jones <jones.shawn.m@gmail.com>
2021-11-24 12:08:12 -0700

    fixed defect in synthesize involving file handles

--- hypercane-gui/scripts/synthesize.py
+++ hypercane-gui/scripts/synthesize.py
@@ -1,12 +1,16 @@
 import os
 import zipfile
 import shutil
+import argparse
+
+from argparse import RawTextHelpFormatter
 
 import hypercane.actions.identify
+from hypercane.actions.synthesize import raintale_story
 import hypercane.errors
 
 from hypercane.args import universal_gui_required_args, universal_gui_optional_args
-from hypercane.args.synthesize import synthesize_parser
+from hypercane.args.synthesize import subparsers_and_arguments
 from hypercane.version import __useragent__
 from hypercane.actions import get_logger, calculate_loglevel
 from hypercane.utils import get_hc_cache_storage
@@ -17,33 +21,52 @@ def zipdir(path, ziph):
         for file in files:
             ziph.write(os.path.join(root, file))
 
+synthesize_parser = argparse.ArgumentParser(prog="hc synthesize",
+    description="'synthesize' synthesizes a web archive collection into other formats, like WARC, JSON, or a set of files in a directory.",
+    formatter_class=RawTextHelpFormatter
+)
+
 if __name__ == '__main__':
 
-    for item in synthesize_parser._subparsers._group_actions:
-        for key in item.choices:
+    subparsers = synthesize_parser.add_subparsers(help='synthesizing outputs', dest="synthesizing output (e.g., warcs, raintale-story)")
+    subparsers.required = True
 
-            subparser = item.choices[key]
+    for subparser_name in subparsers_and_arguments:
 
-            # Wooey's install script does not know how to handle functions, so we have to repeat this
-            required = subparser.add_argument_group('required arguments')
+        # if this doesn't exist, we don't include the subparser for the WUI
+        if 'wui-arguments' in subparsers_and_arguments[subparser_name]:
+
+            subparser = subparsers.add_parser(
+                subparser_name,
+                help=subparsers_and_arguments[subparser_name]['help']
+            )
+            defaults_definition = subparsers_and_arguments[subparser_name]['set_defaults']
+            subparser.set_defaults(
+                which=defaults_definition['which'],
+                output_extension=defaults_definition['output_extension'],
+                exec=defaults_definition['exec']
+            )
+
+            for argument in subparsers_and_arguments[subparser_name]['wui-arguments']:
+                flags = argument['flags']
+                argument_params = argument['argument_params']
+                subparser.add_argument(*flags, **argument_params)
 
             for entry in universal_gui_required_args:
                 flags = entry['flags']
                 argument_params = entry['argument_params']
-                required.add_argument(*flags, **argument_params)
+                subparser.add_argument(*flags, **argument_params)
 
-            optional = subparser.add_argument_group('optional arguments')
             for entry in universal_gui_optional_args:
                 flags = entry['flags']
                 argument_params = entry['argument_params']
-                optional.add_argument(*flags, **argument_params)
+                subparser.add_argument(*flags, **argument_params)
 
-    args = synthesize_parser.parse_args()
+    args = synthesize_parser.parse_args() 
 
     # setting expected arguments for GUI
     if args.output_extension == 'directory':
         vars(args)['output_directory'] = "hypercane-synthesize-output"
-        # TODO: compress directory into a single file?
     else:
         vars(args)['output_filename'] = "hypercane-synthesize-output{}".format(args.output_extension)      
 
@@ -51,6 +74,49 @@ if __name__ == '__main__':
     vars(args)['errorfilename'] = "hypercane-errors.dat"
     vars(args)['cache_storage'] = get_hc_cache_storage()
     vars(args)['input_arguments'] = args.input_file.name
+
+    from datetime import datetime
+    with open("newfile-{}.dat".format(datetime.now()), 'w') as f:
+        f.write(str(datetime.now()))
+
+    # TODO: the other files for synthesize and combine need to be updated to use their filenames instead of their file handles
+    for raintale_story_filearg in [ 'collection_metadata_filename', 'entitydata_filename', 'termdata_filename', 'imagedata_filename' ]:
+        print("searching for {} in the input".format(raintale_story_filearg))
+        if raintale_story_filearg in vars(args):
+            if vars(args)[raintale_story_filearg] is not None:
+                print("adding {} data file {} to the list of files to include in the Raintale story file".format(
+                    raintale_story_filearg,
+                    vars(args)[raintale_story_filearg].name
+                ), flush=True)
+                fh = vars(args)[raintale_story_filearg]
+                vars(args)[raintale_story_filearg] = fh.name
+                fh.close()
+
+    if 'extra_data' in vars(args):
+        print("found extra data files")
+        if vars(args)['extra_data'] is not None:
+            filename_list = []
+            for fh in vars(args)['extra_data']:
+                print("adding extra data file {} to the list of files to include in the Raintale story file".format(fh.name), flush=True)
+                filename_list.append(fh.name)
+                fh.close()
+            vars(args)['extra_data'] = filename_list
+        else:
+            print("setting extra data file list to a default empty list")
+            vars(args)['extra_data'] = []
+
+    else:
+        print("setting extra data file list to a default empty list")
+        vars(args)['extra_data'] = []
+
+    if 'append_files' in vars(args):
+        if vars(args)['append_files'] is not None:
+            filename_list = []
+            for fh in vars(args)['append_files']:
+                print("appending file {} to the list of files to combine".format(fh.name), flush=True)
+                filename_list.append(fh.name)
+                fh.close()
+            vars(args)['append_files'] = filename_list
 
     logger = get_logger(
         __name__,
--- hypercane/args/synthesize.py
+++ hypercane/args/synthesize.py
@@ -69,7 +69,7 @@ raintalestory_parser.add_argument('--entitydata', dest='entitydata_filename',
 )
 
 raintalestory_parser.add_argument('--collection_metadata', dest='collection_metadata_filename',
-    help='A file containing Archive-It colleciton metadata, as produced by hc report metadata',
+    help='A file containing collection metadata, as produced by hc report metadata',
     required=False, default=None
 )
 
@@ -94,7 +94,8 @@ combine_parser.set_defaults(
 
 combine_parser.add_argument('--append-files', dest='append_files',
     help='the Hypercane files to append to the file specified by the -a command',
-    nargs='*'
+    nargs='*',
+    type=argparse.FileType('r')
 )
 
 clusterfree_parser = subparsers.add_parser('cluster-free', help="Strip cluster information out of the input, producing at TSV without any cluster data.")
@@ -103,3 +104,245 @@ clusterfree_parser.set_defaults(
     output_extension='.tsv',
     exec=hypercane.actions.synthesize.remove_clusters
 )
+
+# an experiment with setting subparsers via data and code rather than just code
+subparsers_and_arguments = {
+    'warcs': {
+        'help': "Create WARCs from the mementos in the input.",
+        'set_defaults': {
+            'which': 'warcs',
+            'output_extension': 'directory',
+            'exec': hypercane.actions.synthesize.synthesize_warcs
+        },
+        'cli-arguments': [
+            {   'flags': [ '--no-download-embedded' ],
+                'argument_params' : {
+                    'dest': 'no_download_embedded',
+                    'help': "issue this argument to avoid synthesizing a WARC with embedded images, JavaScript, and stylesheets",
+                    'required': False,
+                    'default': False,
+                    'action': 'store_true'
+                }
+            }
+        ],
+        'wui-arguments': [
+            { 'flags': [ '--no-download-embedded' ],
+                'argument_params' : {
+                    'dest': 'no_download_embedded',
+                    'help': "issue this argument to avoid synthesizing a WARC with embedded images, JavaScript, and stylesheets",
+                    'required': False,
+                    'default': False,
+                    'action': 'store_true'
+                }
+            }
+        ]
+    },
+    'files': {
+        'help': "Save copies of mementos as files from the mementos in the input.",
+        'set_defaults': {
+            'which': 'files',
+            'output_extension': 'directory',
+            'exec': hypercane.actions.synthesize.synthesize_files
+        },
+        'cli-arguments': [],
+        'wui-arguments': []
+    },
+    'bpfree-files': {
+        'help': "Save boilerplate-free files of the mementos in the input.",
+        'set_defaults': {
+            'which': 'bpfree-files',
+            'output_extension': 'directory',
+            'exec': hypercane.actions.synthesize.synthesize_bpfree_files
+        },
+        'cli-arguments': [],
+        'wui-arguments': []
+    },
+    'raintale-story': {
+        'help': "Generate a story suitable for input to Raintale.",
+        'set_defaults': {
+            'which': 'raintale-story',
+            'output_extension': '.json',
+            'exec': hypercane.actions.synthesize.raintale_story
+        },
+        'wui-arguments': [
+            { 'flags' : [ '--title' ],
+                'argument_params' : {
+                    'dest': 'title',
+                    'help': 'The title of the story',
+                    'required': False,
+                    'default': None
+                }
+            },
+            { 'flags': [ '--imagedata' ],
+                'argument_params' : {
+                    'dest': 'imagedata_filename',
+                    'help': 'A file containing image data, as produced by a Hypercane-->report-->Image-Data',
+                    'required': False,
+                    'default': None,
+                    'type': argparse.FileType('r')
+                }
+            },
+            { 'flags': [ '--termdata' ],
+                'argument_params' : {
+                    'dest': 'termdata_filename',
+                    'help': 'A file containing term data, as produced by a Hypercane-->report-->Terms',
+                    'required': False,
+                    'default': None,
+                    'type': argparse.FileType('r')
+                }
+            },
+            { 'flags': [ '--term-count' ],
+                'argument_params' : {
+                    'dest': 'term_count',
+                    'help': 'The number of top terms to select from the term data.',
+                    'required': False,
+                    'default': 5,
+                    'type': int
+                }
+            },
+            { 'flags': [ '--entitydata' ],
+                'argument_params' : {
+                    'dest': 'entitydata_filename',
+                    'help': 'A file containing term data, as produced by a Hypercane-->report-->Entities',
+                    'required': False,
+                    'default': None,
+                    'type': argparse.FileType('r')
+                }
+            },
+            { 'flags': [ '--entity-count' ],
+                'argument_params' : {
+                    'dest': 'entity_count',
+                    'help': 'The number of top terms to select from the term data.',
+                    'required': False,
+                    'default': 5,
+                    'type': int
+                }
+            },
+            { 'flags': [ '--collection_metadata' ],
+                'argument_params' : {
+                    'dest': 'collection_metadata_filename',
+                    'help': 'A file containing collection metadata, as produced by hc report metadata',
+                    'required': False,
+                    'default': None,
+                    'type': argparse.FileType('r')
+                }
+            },
+            { 'flags': [ '--extradata' ],
+                'argument_params' : {
+                    'dest': 'extra_data',
+                    'help': 'a JSON file containing extra key-value pairs that will be included in the Raintale JSON, the name of the file without the extension will be the JSON key',
+                    'nargs': '*',
+                    'type': argparse.FileType('r')
+                }
+            }
+        ],
+        'cli-arguments': [
+            { 'flags' : [ '--title' ],
+                'argument_params' : {
+                    'dest': 'title',
+                    'help': 'The title of the story',
+                    'required': False,
+                    'default': None
+                }
+            },
+            { 'flags': [ '--imagedata' ],
+                'argument_params' : {
+                    'dest': 'imagedata_filename',
+                    'help': 'A file containing image data, as produced by hc report image-data',
+                    'required': False,
+                    'default': None
+                }
+            },
+            { 'flags': [ '--termdata' ],
+                'argument_params' : {
+                    'dest': 'termdata_filename',
+                    'help': 'A file containing term data, as produced by hc report terms',
+                    'required': False,
+                    'default': None
+                }
+            },
+            { 'flags': [ '--term-count' ],
+                'argument_params' : {
+                    'dest': 'term_count',
+                    'help': 'The number of top terms to select from the term data.',
+                    'required': False,
+                    'default': 5,
+                    'type': int
+                }
+            },
+            { 'flags': [ '--entitydata' ],
+                'argument_params' : {
+                    'dest': 'entitydata_filename',
+                    'help': 'A file containing term data, as produced by hc report entities',
+                    'required': False,
+                    'default': None
+                }
+            },
+            { 'flags': [ '--entity-count' ],
+                'argument_params' : {
+                    'dest': 'entity_count',
+                    'help': 'The number of top terms to select from the term data.',
+                    'required': False,
+                    'default': 5,
+                    'type': int
+                }
+            },
+            { 'flags': [ '--collection_metadata' ],
+                'argument_params' : {
+                    'dest': 'collection_metadata_filename',
+                    'help': 'A file containing collection metadata, as produced by hc report metadata',
+                    'required': False,
+                    'default': None
+                }
+            },
+            { 'flags': [ '--extradata' ],
+                'argument_params' : {
+                    'dest': 'extra_data',
+                    'help': 'a JSON file containing extra key-value pairs that will be included in the Raintale JSON, multiple filenames may follow this argument, the name of the file without the extension will be the JSON key',
+                    'default': [],
+                }
+            }
+        ]
+    },
+    'combine': {
+        'help': "Combine the output from several Hypercane commands into one TSV file.",
+        'set_defaults': {
+            'which': 'combine',
+            'output_extension': '.tsv',
+            'exec': hypercane.actions.synthesize.combine_files
+        },
+        'cli-arguments': [
+            { 'flags': [ '--append-files' ],
+                'argument_params' : {
+                    'dest': 'append_files',
+                    'help': 'the files to append to the input file',
+                    'required': False,
+                    'nargs': '*',
+                    'type': argparse.FileType('r')
+                }
+            }
+        ],
+        'wui-arguments': [
+            { 'flags': [ '--append-files' ],
+                'argument_params' : {
+                    'dest': 'append_files',
+                    'help': 'the files to append to the input file',
+                    'required': False,
+                    'nargs': '*',
+                    'type': argparse.FileType('r')
+                }
+            }
+        ]
+    },
+    'cluster-free': {
+        'help': "Strip cluster information out of the input, producing at TSV without any cluster data.",
+        'set_defaults': {
+            'which': 'cluster-free',
+            'output_extension': '.tsv',
+            'exec': hypercane.actions.synthesize.remove_clusters
+        },
+        'cli-arguments': [],
+        'wui-arguments': []
+    }
+
+}
