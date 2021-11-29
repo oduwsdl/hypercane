030ac53bad5d5f5aca00f86a6ed9057b2c993a7f
Shawn M. Jones <jones.shawn.m@gmail.com>
2021-11-28 21:21:29 -0700

    committing version 0.5.20211129042129 before taggin

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -26,7 +26,7 @@ author = u'Shawn M. Jones'
 # The short X.Y version
 version = u''
 # The full version, including alpha/beta/rc tags
-release = u'0.5.20211127223403'
+release = u'0.5.20211129042129'
 
 # -- General configuration ---------------------------------------------------
 
