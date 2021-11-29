5c9203a7d306c4f28eb54c90d98a9ffe6ea64eed
Shawn M. Jones <jones.shawn.m@gmail.com>
2021-11-28 22:07:23 -0700

    committing version 0.5.20211129050722 before taggin

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -26,7 +26,7 @@ author = u'Shawn M. Jones'
 # The short X.Y version
 version = u''
 # The full version, including alpha/beta/rc tags
-release = u'0.5.20211129042129'
+release = u'0.5.20211129050722'
 
 # -- General configuration ---------------------------------------------------
 
--- hypercane/version.py
+++ hypercane/version.py
@@ -1,3 +1,3 @@
 __appname__ = "hypercane"
-__appversion__ = '0.5.20211129042129'
+__appversion__ = '0.5.20211129050722'
 __useragent__ = "{}/{}".format(__appname__, __appversion__)
